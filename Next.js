6600bca3e9af97ee67ed08466069a05a159d344c
Talha <133606755+Talha017455@users.noreply.github.com>
2025-05-09 11:07:54 +0600

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,20 @@
+{
+  "name": "চাঁদা হিসাব অ্যাপ",
+  "short_name": "চাঁদা হিসাব",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "icons": [
+    {
+      "src": "/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
